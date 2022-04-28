8cf2dfc80b3d380aaa178deec134713d4e66fa68
Subhonbek <basubhonbek@gmail.com>
2022-04-28 16:22:39 +0300

    homeworkOne

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import staly from './Message.module.css'
 
-function Message() {
+interface IProps {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
+function Message(props: IProps) {
     return (
         <div>
-
+            <div className={staly.message}>
+                <img src={props.avatar} alt="avatar" />
+                <div className={staly.d33}>
+                    <div>
+                        <div>{props.name}</div>
+                        <div className={staly.text}>{props.message}</div>
+                    </div>
+                    <div className={staly.time}>{props.time}</div>
+                </div>
+            </div>
         </div>
     )
 }
